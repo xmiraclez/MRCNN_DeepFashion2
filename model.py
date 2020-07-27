dea4208e025b05aca9f07cbedf99200f1acc0569
o-evgeny <o-evgeny@users.noreply.github.com>
2020-07-27 19:54:41 +0500

    Initial

---
+++ model.py
@@ -0,0 +1,333 @@
+import sys
+sys.dont_write_bytecode = True
+import os
+import numpy as np
+from pycocotools.coco import COCO
+from pycocotools import mask as maskUtils
+from mrcnn.config import Config
+from krcnn.model import MaskRCNN
+from mrcnn import utils
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+import matplotlib.pyplot as plt
+from mrcnn.visualize import * 
+from mrcnn.visualize import display_instances
+from mrcnn import visualize
+import skimage 
+import cv2
+import uuid
+
+
+
+
+class DeepFashion2Config(Config):
+
+    # Give the configuration a recognizable name
+    NAME = "main_deepfashion2_sgd_bs8_2"
+    
+    EPCHS=90
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 8
+
+    # Uncomment to train on 8 GPUs (default is 1)
+    GPU_COUNT = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 13  # COCO has 80 classes
+    
+    USE_MINI_MASK = True
+
+    train_img_dir = "/home/test/data/train/image/"
+    train_json_path = "/home/test/data/train/train.json"
+    valid_img_dir = "/home/test/data/validation/image/"
+    valid_json_path = "/home/test/data/validation/valid.json"
+
+
+############################################################
+#  Dataset
+############################################################
+
+class DeepFashion2Dataset(utils.Dataset):
+    def load_coco(self, image_dir, json_path, class_ids=None,
+                  class_map=None, return_coco=False):
+
+        coco = COCO(json_path)
+
+        # Load all classes or a subset?
+        if not class_ids:
+            # All classes
+            class_ids = sorted(coco.getCatIds())
+
+        # All images or a subset?
+        if class_ids:
+            image_ids = []
+            for id in class_ids:
+                image_ids.extend(list(coco.getImgIds(catIds=[id])))
+            # Remove duplicates
+            image_ids = list(set(image_ids))
+        else:
+            # All images
+            image_ids = list(coco.imgs.keys())
+
+        # Add classes
+        for i in class_ids:
+            self.add_class("deepfashion2", i, coco.loadCats(i)[0]["name"])
+
+        # Add images
+        for i in image_ids:
+            self.add_image(
+                "deepfashion2", image_id=i,
+                path=os.path.join(image_dir, coco.imgs[i]['file_name']),
+                width=coco.imgs[i]["width"],
+                height=coco.imgs[i]["height"],
+                annotations=coco.loadAnns(coco.getAnnIds(
+                    imgIds=[i], catIds=class_ids, iscrowd=None)))
+        if return_coco:
+            return coco
+
+    def load_keypoint(self, image_id):
+        """
+        """
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "deepfashion2":
+            return super(DeepFashion2Dataset, self).load_mask(image_id)
+
+        instance_keypoints = []
+        class_ids = []
+        annotations = self.image_info[image_id]["annotations"]
+
+        for annotation in annotations:
+            class_id = self.map_source_class_id(
+                "deepfashion2.{}".format(annotation['category_id']))
+            if class_id:
+                keypoint = annotation['keypoints']
+
+                instance_keypoints.append(keypoint)
+                class_ids.append(class_id)
+
+        keypoints = np.stack(instance_keypoints, axis=1)
+        class_ids = np.array(class_ids, dtype=np.int32)
+        return keypoints, class_ids
+            
+    def load_mask(self, image_id):
+        """Load instance masks for the given image.
+        Different datasets use different ways to store masks. This
+        function converts the different mask format to one format
+        in the form of a bitmap [height, width, instances].
+        Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not a COCO image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "deepfashion2":
+            return super(DeepFashion2Dataset, self).load_mask(image_id)
+
+        instance_masks = []
+        class_ids = []
+        annotations = self.image_info[image_id]["annotations"]
+        # Build mask of shape [height, width, instance_count] and list
+        # of class IDs that correspond to each channel of the mask.
+        for annotation in annotations:
+            class_id = self.map_source_class_id(
+                "deepfashion2.{}".format(annotation['category_id']))
+            if class_id:
+                m = self.annToMask(annotation, image_info["height"],
+                                   image_info["width"])
+                # Some objects are so small that they're less than 1 pixel area
+                # and end up rounded out. Skip those objects.
+                if m.max() < 1:
+                    continue
+                # Is it a crowd? If so, use a negative class ID.
+                if annotation['iscrowd']:
+                    # Use negative class ID for crowds
+                    class_id *= -1
+                    # For crowd masks, annToMask() sometimes returns a mask
+                    # smaller than the given dimensions. If so, resize it.
+                    if m.shape[0] != image_info["height"] or m.shape[1] != image_info["width"]:
+                        m = np.ones([image_info["height"], image_info["width"]], dtype=bool)
+                instance_masks.append(m)
+                class_ids.append(class_id)
+
+        # Pack instance masks into an array
+        if class_ids:
+            mask = np.stack(instance_masks, axis=2).astype(np.bool)
+            class_ids = np.array(class_ids, dtype=np.int32)
+            return mask, class_ids
+        else:
+            # Call super class to return an empty mask
+            return super(DeepFashion2Dataset, self).load_mask(image_id)
+
+    def image_reference(self, image_id):
+        """Return a link to the image in the COCO Website."""
+        super(DeepFashion2Dataset, self).image_reference(image_id)
+
+    # The following two functions are from pycocotools with a few changes.
+
+    def annToRLE(self, ann, height, width):
+        """
+        Convert annotation which can be polygons, uncompressed RLE to RLE.
+        :return: binary mask (numpy 2D array)
+        """
+        segm = ann['segmentation']
+        if isinstance(segm, list):
+            # polygon -- a single object might consist of multiple parts
+            # we merge all parts into one mask rle code
+            rles = maskUtils.frPyObjects(segm, height, width)
+            rle = maskUtils.merge(rles)
+        elif isinstance(segm['counts'], list):
+            # uncompressed RLE
+            rle = maskUtils.frPyObjects(segm, height, width)
+        else:
+            # rle
+            rle = ann['segmentation']
+        return rle
+
+    def annToMask(self, ann, height, width):
+        """
+        Convert annotation which can be polygons, uncompressed RLE, or RLE to binary mask.
+        :return: binary mask (numpy 2D array)
+        """
+        rle = self.annToRLE(ann, height, width)
+        m = maskUtils.decode(rle)
+        return m
+
+
+def train(model, config):
+    """
+    """
+    dataset_train = DeepFashion2Dataset()
+    dataset_train.load_coco(config.train_img_dir, config.train_json_path)
+    dataset_train.prepare()
+
+    dataset_valid = DeepFashion2Dataset()
+    dataset_valid.load_coco(config.valid_img_dir, config.valid_json_path)
+    dataset_valid.prepare()
+
+    model.train(dataset_train, dataset_valid,
+                learning_rate=config.LEARNING_RATE,
+                epochs=DeepFashion2Config.EPCHS,
+                layers='heads')
+
+
+if __name__ == "__main__":
+    ROOT_DIR = os.path.abspath("./")
+    DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+    COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+    import argparse
+
+    print(DEFAULT_LOGS_DIR )
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Match R-CNN for DeepFashion.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'inference'")
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--image', required=False,
+                        metavar="path or URL to image",
+                        help='Image to apply the color splash effect on')
+    parser.add_argument('--video', required=False,
+                        metavar="path or URL to video",
+                        help='Video to apply the color splash effect on')
+    parser.add_argument('--input_folder', required=False)
+    parser.add_argument('--output_folder', required=False)
+    args = parser.parse_args()
+
+    """
+
+    """
+
+    print("Weights: ", args.weights)
+    print("Logs: ", args.logs)
+
+
+    # Configurations
+    if args.command == "train":
+        config = DeepFashion2Config()
+    else:
+        class InferenceConfig(DeepFashion2Config):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+        config = InferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = MaskRCNN(mode="training", config=config,model_dir=args.logs)
+    elif args.command == "inference":
+        model = MaskRCNN(mode="inference", config=config,
+                                  model_dir=args.logs)
+
+    # Select weights file to load
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    elif args.weights.lower() == "last":
+        # Find last trained weights
+        weights_path = model.find_last()
+    elif args.weights.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        weights_path = model.get_imagenet_weights()
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
+
+    # Train or evaluate
+    if args.command == "train":
+        train(model, config)
+    elif args.command == "inference":
+        class_names = ['short_sleeved_shirt', 'long_sleeved_shirt', 'short_sleeved_outwear', 'long_sleeved_outwear', 'vest', 'sling', 
+               'shorts', 'trousers', 'skirt', 'short_sleeved_dress', 'long_sleeved_dress',
+               'vest_dress', 'sling_dress']
+       
+        class TestConfig(Config):
+            NAME = "test"
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+            NUM_CLASSES = 1 + 13
+
+        rcnn = MaskRCNN(mode='inference', model_dir=args.logs, config=TestConfig())
+        rcnn.load_weights(args.weights,by_name=True)
+
+        input_folder=args.input_folder
+        output_folder=args.output_folder
+        if not os.path.isdir(output_folder):
+                        os.makedirs(output_folder)     
+        files = os.listdir(input_folder) 
+        for file in files:
+            print(file)
+            img = skimage.io.imread(input_folder+'/'+file)
+            results = rcnn.detect([img], verbose=1)
+            r = results[0]
+            visualize.display_instances(img, r['rois'], r['masks'], r['class_ids'], class_names, file, output_folder, r['scores'])
+
+
+    else:
+        print("'{}' is not recognized. "
+              "Use 'train' or 'inference'".format(args.command)) 
+
+
